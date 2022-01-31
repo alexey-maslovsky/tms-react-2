f8e1933ef244dc640cd514903b34ef8955fa5db4
Alex Maslovsky <alexey.maslovsky@satelliteinnovations.io>
2022-01-31 17:15:31 +0300

    add changes from lession

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ButtonLink from './components/ButtonLink/ButtonLink';
 import Profile from './components/Profile/Profile';
 import ProfileForm from './components/ProfileForm/ProfileForm';
-
-const TEST_DATA = {
-  firstName: 'Alex',
-  lastName: 'Maslovsky',
-  photoSrc: 'https://cdn.mos.cms.futurecdn.net/VSy6kJDNq2pSXsCzb6cvYF.jpg',
-  hobbies: [
-    { id: '1', name: 'guitar' },
-    { id: '2', name: 'nature' },
-    { id: '3', name: 'gaming' },
-  ],
-};
+import { Hearts } from  'react-loader-spinner';
+import getProfile from './api/getProfile';
+import updateProfile from './api/updateProfile';
 
 const UserProfileContainerStyles = {
   display: 'flex',
@@ -20,26 +12,44 @@ const UserProfileContainerStyles = {
 
 const App = () => {
   const [isEdit, setIsEdit] = useState(false);
+  const [data, setData] = useState(null);
 
   const handleEdit = () => {
     setIsEdit(true);
   };
 
-  const handleSave = () => {
+  const handleSave = async (newProfileData) => {
+    const updatedData = await updateProfile(newProfileData);
+
+    setData(updatedData);
+
     setIsEdit(false);
   };
+
+  const fetchProfileData = async () => {
+    const newData = await getProfile();
+
+    setData(newData);
+  };
+
+  useEffect(() => {
+    fetchProfileData();
+  }, []);
 
   return (
     <div>
       <div style={UserProfileContainerStyles}>
         User Profile{' '}
-        <ButtonLink onClick={isEdit ? handleSave : handleEdit}>
-          {isEdit ? 'Save' : 'Edit'}
-        </ButtonLink>
+        {!isEdit && data && (
+          <ButtonLink onClick={handleEdit}>
+            Edit
+          </ButtonLink>
+        )}
       </div>
 
-      {!isEdit && <Profile user={TEST_DATA} />}
-      {isEdit && <ProfileForm user={TEST_DATA} />}
+      {!data && <Hearts />}
+      {!isEdit && data && <Profile user={data} />}
+      {isEdit && data && <ProfileForm user={data} onSave={handleSave} />}
     </div>
   );
 };
--- src/components/ProfileForm/ProfileForm.js
+++ src/components/ProfileForm/ProfileForm.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import uniqId from 'uniqid';
 
-const ProfileForm = ({ user }) => {
+const ProfileForm = ({ user, onSave }) => {
   const [firstName, setFirstName] = useState(user.firstName);
   const [lastName, setLastName] = useState(user.lastName);
   const [photoSrc, setPhotoSrc] = useState(user.photoSrc);
   const [hobbies, setHobbies] = useState(user.hobbies);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleOnFirstNameChange = (event) => {
     setFirstName(event.target.value);
@@ -28,6 +30,34 @@ const ProfileForm = ({ user }) => {
     setHobbies(newHobbies);
   };
 
+  const handleOnHobbyAdd = () => {
+    setHobbies([
+      ...hobbies,
+      { id: uniqId(), name: '' },
+    ]);
+  };
+
+  const handleOnHobbyDelete = (hobbyToDelete) => {
+    return () => {
+      const filteredHobbies = hobbies.filter((hobby) => hobby.id !== hobbyToDelete.id);
+
+      setHobbies(filteredHobbies);
+    };
+  };
+
+  const handleProfileSave = async () => {
+    setIsLoading(true);
+
+    await onSave({
+      firstName,
+      lastName,
+      photoSrc,
+      hobbies,
+    });
+
+    setIsLoading(false);
+  };
+
   return (
     <div>
       <label>
@@ -47,9 +77,14 @@ const ProfileForm = ({ user }) => {
         {hobbies.map((hobby) => (
           <div key={hobby.id}>
             <input value={hobby.name} onChange={handleOnHobbyChange(hobby)} />
+            <button onClick={handleOnHobbyDelete(hobby)}>X</button>
           </div>
         ))}
+        <button onClick={handleOnHobbyAdd}>Add</button>
       </div>
+      <button onClick={handleProfileSave} disabled={isLoading}>
+        {isLoading ? 'Loading' : 'Save'}
+      </button>
     </div>
   );
 };
